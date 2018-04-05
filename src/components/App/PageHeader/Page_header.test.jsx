86c60d0d8ea4a01abdb66716e2b53352b9cd5c89
Edgar Dewsbery <edewsbery@gmail.com>
2018-04-05 16:31:54 +0100

    snaptshot test page header

---
+++ src/components/App/PageHeader/Page_header.test.jsx
@@ -0,0 +1,21 @@
+/* eslint-env jest */
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+
+import PageHeader from './Page_header';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const setup = () => {
+  const wrapper = shallow(<PageHeader />);
+  return { wrapper };
+};
+
+describe('<PageHeader />', () => {
+  const { wrapper } = setup();
+  it('renders as expected', () => {
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
