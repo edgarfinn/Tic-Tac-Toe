2a3fb5d8ac4df39edb78c01641a4d7eef151c3f6
Edgar Dewsbery <edewsbery@gmail.com>
2018-04-06 17:34:50 +0100

    snapshot test Hash component

---
+++ src/containers/GameBoardContainer/GameBoard/Hash/Hash.test.jsx
@@ -0,0 +1,23 @@
+/* eslint-env jest */
+
+import React from 'react';
+import 'jest-styled-components';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
+import { shallowToJson } from 'enzyme-to-json';
+
+import Hash from './Hash';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+const setup = () => {
+  const wrapper = shallow(<Hash />);
+  return { wrapper };
+};
+
+describe('<Hash />', () => {
+  const { wrapper } = setup();
+  it('renders correctly', () => {
+    expect(shallowToJson(wrapper)).toMatchSnapshot();
+  });
+});
