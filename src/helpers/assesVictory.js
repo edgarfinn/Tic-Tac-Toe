0f83f410bd86679c87a3072293573f7b24013981
Edgar Dewsbery <edewsbery@gmail.com>
2018-08-02 18:31:28 +0100

    initate helpers folder

---
+++ src/helpers/assesVictory.js
@@ -0,0 +1,15 @@
+const winningCombinations = [
+  ['A1', 'A2', 'A3'],
+  ['B1', 'B2', 'B3'],
+  ['C1', 'C2', 'C3'],
+  ['A1', 'B1', 'C1'],
+  ['A2', 'B2', 'C2'],
+  ['A3', 'B3', 'C3'],
+  ['A1', 'B2', 'C3'],
+  ['A3', 'B2', 'C1']
+]
+
+export default occupations => {
+  console.log(occupations)
+  return {'A1': 'crosses', 'A2': 'crosses', 'A3': 'crosses'}
+}
