ad0744ed192b6541937e4be87fad715da5ebbf51
Edgar Dewsbery <edewsbery@gmail.com>
2018-08-02 18:24:11 +0100

    currentPlayer reducer listen for SWITCH_PLAYER action

--- src/reducers/reducer_current-player.js
+++ src/reducers/reducer_current-player.js
@@ -1,8 +1,8 @@
-import { NEW_MOVE } from '../actions'
+import { SWITCH_PLAYERS } from '../constants'
 
 export default (state = 'crosses', action) => {
   switch (action.type) {
-    case NEW_MOVE:
+    case SWITCH_PLAYERS:
       if (state === 'crosses') {
         return 'noughts'
       } return 'crosses'
