8221a848082a11e5d4bdb6c1a0d556098cd4de17
Edgar Dewsbery <edewsbery@gmail.com>
2018-08-02 18:17:22 +0100

    delete actions file

--- src/actions/index.js
+++
@@ -1,8 +0,0 @@
-export const NEW_MOVE = 'NEW_MOVE'
-
-export const newMove = (occupations, currentPlayer, cell) => (
-  {
-    type: NEW_MOVE,
-    payload: { ...occupations, [cell]: currentPlayer }
-  }
-)
